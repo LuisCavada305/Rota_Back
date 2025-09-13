1b67c439d3dbc6d0012d38e1b9297f28f3f8d621
LuisCavada305 <lhcavada@gmail.com>
2025-09-13 11:10:08 -0300

    ajuste

--- app/repositories/UsersRepository.py
+++ app/repositories/UsersRepository.py
@@ -2,12 +2,13 @@ from sqlalchemy.orm import Session
 from typing import Optional
 from app.models.users import User
 
+
 class UsersRepository:
     def __init__(self, db: Session):
         self.db = db
 
     def GetUserByEmail(self, email: str) -> Optional[User]:
         return self.db.query(User).filter(User.email == email).first()
-    
+
     def GetUserByUsername(self, username: str) -> Optional[User]:
         return self.db.query(User).filter(User.username == username).first()
