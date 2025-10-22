12d43b648415e09891f24e4b414de4ddaa593df9
LuisCavada305 <lhcavada@gmail.com>
2025-10-22 19:38:37 -0300

    ajustes

--- app/repositories/UserProgressRepository.py
+++ app/repositories/UserProgressRepository.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from sqlalchemy import func
+from sqlalchemy import text
 from typing import Optional
 
 from app.models.user_item_progress import UserItemProgress as UserItemProgressORM
@@ -32,45 +32,126 @@ class UserProgressRepository:
     ):
         status_id = self._status_id(status_code)
         completed_status_id = self._status_id("COMPLETED")
-        uip = (
-            self.db.query(UserItemProgressORM)
-            .filter(
-                UserItemProgressORM.user_id == user_id,
-                UserItemProgressORM.trail_item_id == item_id,
-            )
-            .first()
+        sanitized_progress = max(0, progress_value) if progress_value is not None else None
+        progress_update_flag = sanitized_progress is not None
+        last_passed_update_flag = last_passed_submission_id is not None
+
+        sanitized_progress = max(0, progress_value) if progress_value is not None else None
+
+        now_sql = text("SELECT now(), now()")
+        now_result = self.db.execute(now_sql).one()
+        current_now = now_result[0]
+        current_now_utc = now_result[1]
+
+        update_sql = text(
+            """
+            UPDATE user_item_progress
+            SET
+                status_id = CASE
+                    WHEN user_item_progress.status_id = :completed_status_id
+                        THEN user_item_progress.status_id
+                    ELSE :status_id
+                END,
+                progress_value = CASE
+                    WHEN :update_progress
+                        THEN GREATEST(
+                            COALESCE(user_item_progress.progress_value, 0),
+                            :sanitized_progress_value
+                        )
+                    ELSE user_item_progress.progress_value
+                END,
+                last_passed_submission_id = CASE
+                    WHEN :update_last_passed
+                        THEN :last_passed_submission_id
+                    ELSE user_item_progress.last_passed_submission_id
+                END,
+                last_interaction = :current_now,
+                last_interaction_utc = :current_now_utc,
+                completed_at = CASE
+                    WHEN user_item_progress.status_id = :completed_status_id
+                        THEN user_item_progress.completed_at
+                    WHEN :status_id = :completed_status_id
+                        THEN :current_now
+                    ELSE NULL
+                END,
+                completed_at_utc = CASE
+                    WHEN user_item_progress.status_id = :completed_status_id
+                        THEN user_item_progress.completed_at_utc
+                    WHEN :status_id = :completed_status_id
+                        THEN :current_now_utc
+                    ELSE NULL
+                END
+            WHERE user_item_progress.user_id = :user_id
+              AND user_item_progress.trail_item_id = :trail_item_id
+            RETURNING id
+            """
         )
-        if not uip:
-            uip = UserItemProgressORM(
-                user_id=user_id,
-                trail_item_id=item_id,
+
+        update_params = {
+            "user_id": user_id,
+            "trail_item_id": item_id,
+            "status_id": status_id,
+            "completed_status_id": completed_status_id,
+            "update_progress": sanitized_progress is not None,
+            "sanitized_progress_value": sanitized_progress if sanitized_progress is not None else 0,
+            "update_last_passed": last_passed_submission_id is not None,
+            "last_passed_submission_id": last_passed_submission_id,
+            "current_now": current_now,
+            "current_now_utc": current_now_utc,
+        }
+
+        result = self.db.execute(update_sql, update_params)
+        uip_id = result.scalar_one_or_none()
+
+        if uip_id is None:
+            insert_sql = text(
+                """
+                INSERT INTO user_item_progress (
+                    user_id,
+                    trail_item_id,
+                    status_id,
+                    progress_value,
+                    last_interaction,
+                    completed_at,
+                    last_interaction_utc,
+                    completed_at_utc,
+                    last_passed_submission_id
+                )
+                VALUES (
+                    :user_id,
+                    :trail_item_id,
+                    :status_id,
+                    :insert_progress_value,
+                    :current_now,
+                    CASE WHEN :status_id = :completed_status_id THEN :current_now ELSE NULL END,
+                    :current_now_utc,
+                    CASE WHEN :status_id = :completed_status_id THEN :current_now_utc ELSE NULL END,
+                    :insert_last_passed_submission_id
+                )
+                ON CONFLICT (user_id, trail_item_id) DO NOTHING
+                RETURNING id
+                """
             )
-            self.db.add(uip)
 
-        if uip.status_id == completed_status_id and status_code != "COMPLETED":
-            status_code = "COMPLETED"
-            status_id = completed_status_id
-        else:
-            uip.status_id = status_id
+            insert_params = {
+                "user_id": user_id,
+                "trail_item_id": item_id,
+                "status_id": status_id,
+                "completed_status_id": completed_status_id,
+                "insert_progress_value": sanitized_progress if sanitized_progress is not None else None,
+                "insert_last_passed_submission_id": last_passed_submission_id,
+                "current_now": current_now,
+                "current_now_utc": current_now_utc,
+            }
 
-        if progress_value is not None:
-            prev_value = uip.progress_value or 0
-            uip.progress_value = max(prev_value, max(0, progress_value))
-        if last_passed_submission_id is not None:
-            uip.last_passed_submission_id = last_passed_submission_id
+            insert_result = self.db.execute(insert_sql, insert_params)
+            uip_id = insert_result.scalar_one_or_none()
 
-        now_expr = func.now()
-        uip.last_interaction = now_expr
-        uip.last_interaction_utc = now_expr
+            if uip_id is None:
+                result = self.db.execute(update_sql, update_params)
+                uip_id = result.scalar_one()
 
-        if status_code == "COMPLETED":
-            uip.completed_at = now_expr
-            uip.completed_at_utc = now_expr
-        elif uip.status_id != completed_status_id:
-            uip.completed_at = None
-            uip.completed_at_utc = None
-
-        self.db.flush()
+        uip = self.db.get(UserItemProgressORM, uip_id)
 
         trail_id = (
             self.db.query(TrailItemsORM.trail_id)
